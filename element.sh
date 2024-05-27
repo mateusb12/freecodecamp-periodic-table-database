04e380fc8962a45798b72b9b598be6dfcdd3ee07
mateusb12 <mateus_b09@hotmail.com>
2024-05-27 17:14:27 +0000

    Initial commit

---
+++ element.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# PSQL command to interact with the database
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+# Function to fetch and display element information
+fetch_element_info() {
+  local QUERY_RESULT=$($PSQL "SELECT elements.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius
+                             FROM elements
+                             INNER JOIN properties USING(atomic_number)
+                             INNER JOIN types ON properties.type_id = types.type_id
+                             WHERE elements.atomic_number=$1 OR elements.symbol='$1' OR elements.name='$1'")
+
+  if [[ -z $QUERY_RESULT ]]; then
+    echo "I could not find that element in the database."
+  else
+    echo "$QUERY_RESULT" | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT; do
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    done
+  fi
+}
+
+# Check if argument is provided
+if [[ -z $1 ]]; then
+  echo "Please provide an element as an argument."
+  exit 0
+fi
+
+# Fetch element information based on the input
+fetch_element_info "$1"
